35daf1db48ca744730f5546ade38355edaf272a5
Gage Kornexl <gagekornexl@yahoo.com>
2019-09-10 14:33:53 -0400

    Adds conditional code to banner so we can import it

--- banner.py
+++ banner.py
@@ -1,18 +1,17 @@
-print("=============================")
-print("             Banner          ")
-print("             By Gage         ")
-print("=============================")
-print("")
 
 def banner(title, author):
     title_length = len(title)
-    banner_length = title_length + 8
-    byline = f"By {author}"
-    print("===================================")
-    print(f"{title}         ")
-    print(f"              By {author}      ")
-    print("===================================")
-    banner_length = title_length + 8
+    by_line = f"By {author}"
+    by_line_length = len(by_line)
+    banner_length = max(title_length, by_line_length) + 4
+    print(f"{'=' * banner_length}")
+    print(f"{title.upper():^{banner_length}}")
+    print(f"{by_line:^{banner_length}}")
+    print(f"{'=' * banner_length}")
     print("")
 
-banner("sub to my youtube channel Gamerman", "Gage")
+if __name__ == "__main__":
+    banner("BANNER", "Mr. Baldus")
+    author = input("What is your name?")
+    title = input("What is your quest?")
+    banner(title, author)
